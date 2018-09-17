0752e487e8708143bb4ca0e55e647f1f90c03e19
Kishore <kishorek@thoughtworks.com>
2018-09-17 18:12:52 +0200

    Initial commit with a sample test

---
+++ build.py
@@ -0,0 +1,6 @@
+from pybuilder.core import use_plugin
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+
+default_task = "publish"
---
+++ src/unittest/python/httpparser_tests.py
@@ -0,0 +1,5 @@
+import unittest
+
+class HttpParseTest(unittest.TestCase):
+  def test_sample_test(self):
+    self.assertTrue(True)

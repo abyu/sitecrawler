3a55c424e197179c1aa83498ccbd706f9032b114
Kishore <kishorek@thoughtworks.com>
2018-09-19 18:52:05 +0200

    Add CrawlerRules class to filter url based on multiple filters

--- src/main/python/crawler/urlfilter.py
+++ src/main/python/crawler/urlfilter.py
@@ -14,3 +14,15 @@ class DuplicateUrlFilter:
 
   def filter_links(self, links):
     return list(filter(lambda link: not link.is_same_as(self.url), links))
+
+class CrawlerRules:
+
+  def __init__(self, filters):
+    self.filters = filters
+
+  def apply_rules(self, links):
+    filtered_links = links
+    for filter in self.filters:
+      filtered_links = filter.filter_links(filtered_links)
+
+    return filtered_links
--- src/unittest/python/url_filter_tests.py
+++ src/unittest/python/url_filter_tests.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from crawler.urlfilter import SameDomainUrlFilter, DuplicateUrlFilter
 from crawler.link import Link
+from crawler.urlfilter import CrawlerRules
+from mockito import mock, when
 
 class SameDomainUrlFilterTest(TestCase):
 
@@ -71,5 +73,71 @@ class DuplicateUrlFilterTest(TestCase):
     links = []
 
     filtered_links = filter.filter_links(links)
+
+    self.assertEquals(0, len(filtered_links))
+
+class CrawlerRulesTest(TestCase):
+
+  def test_returns_urls_that_satifies_all_filters(self):
+    mock_filter1 = mock()
+    mock_filter2 = mock()
+    filters = [mock_filter1, mock_filter2]
+    links = [Link(url = "", label="", parent_url=""), Link(url = "", label="", parent_url="")]
+    when(mock_filter1).filter_links(links).thenReturn(links)
+    when(mock_filter2).filter_links(links).thenReturn(links)
+    rule = CrawlerRules(filters)
+
+    filtered_links = rule.apply_rules(links)
+
+    self.assertEquals(2, len(filtered_links))
+    self.assertCountEqual(links, filtered_links)
+
+  def test_pass_filtered_urls_from_one_filter_to_the_next_one(self):
+    mock_filter1 = mock()
+    mock_filter2 = mock()
+    filters = [mock_filter1, mock_filter2]
+    link = Link(url = "", label="", parent_url="")
+    link2 = Link(url = "", label="", parent_url="")
+    link_unsatisfying_filter1 = Link(url = "", label="", parent_url="")
+    links = [link, link2, link_unsatisfying_filter1]
+    when(mock_filter1).filter_links(links).thenReturn([link, link2])
+    when(mock_filter2).filter_links([link, link2]).thenReturn([link])
+    rule = CrawlerRules(filters)
+
+    filtered_links = rule.apply_rules(links)
+
+    self.assertEquals(1, len(filtered_links))
+    self.assertCountEqual([link], filtered_links)
+
+  def test_remove_url_that_dont_satisfy_even_one_filter(self):
+    mock_filter1 = mock()
+    mock_filter2 = mock()
+    filters = [mock_filter1, mock_filter2]
+    link = Link(url = "", label="", parent_url="")
+    unsatifying_link = Link(url = "", label="", parent_url="")
+
+    links = [link, unsatifying_link]
+    when(mock_filter1).filter_links(links).thenReturn(links)
+    when(mock_filter2).filter_links(links).thenReturn([link])
+    rule = CrawlerRules(filters)
+
+    filtered_links = rule.apply_rules(links)
+
+    self.assertEquals(1, len(filtered_links))
+    self.assertCountEqual([link], filtered_links)
+
+  def test_return_empty_when_no_urls_match_the_rules(self):
+    mock_filter1 = mock()
+    mock_filter2 = mock()
+    filters = [mock_filter1, mock_filter2]
+    unsatifying_link1 = Link(url = "", label="", parent_url="")
+    unsatifying_link2 = Link(url = "", label="", parent_url="")
+
+    links = [unsatifying_link1, unsatifying_link2]
+    when(mock_filter1).filter_links(links).thenReturn([])
+    when(mock_filter2).filter_links([]).thenReturn([])
+    rule = CrawlerRules(filters)
+
+    filtered_links = rule.apply_rules(links)
 
     self.assertEquals(0, len(filtered_links))

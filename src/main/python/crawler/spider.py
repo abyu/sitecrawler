c301970c4ee00408353570241937c57c0225e6e3
Kishore <kishorek@thoughtworks.com>
2018-09-19 18:27:20 +0200

    Add Link scraper to extract all links from a page

--- src/main/python/crawler/spider.py
+++ src/main/python/crawler/spider.py
@@ -1,23 +1,23 @@
 from crawler.http_client import HTTPClient
 from crawler.html_parser import HtmlParser
+from crawler.urlfilter import SameDomainUrlFilter, DuplicateUrlFilter
 
 class Spider():
 
-  def get_all_links(self, url):
-    client = HTTPClient()
-    parser = HtmlParser()
+  def __init__(self):
+    pass
 
-    page_content = client.get_html_page(url)
+class LinkScraper():
+
+  def __init__(self, http_client, html_parser):
+    self.http_client = http_client
+    self.html_parser = html_parser
+
+  def scrape_links(self, url):
+    page_content = self.http_client.get_html_page(url)
     if page_content:
-      doc = parser.parse(url, page_content)
-      return doc.get_links()
-    return None
+      document = self.html_parser.parse(url, page_content)
+      return document.get_links()
+    return []
 
-  def get(self, url):
-    links = self.get_all_links(url)
-
-    # apply filters
-    # sub_links = list(map(lambda link: self.get(link.url), links))
-
-    return {"parent":  url, "links": links}
 
--- src/unittest/python/spider_tests.py
+++ src/unittest/python/spider_tests.py
@@ -1,8 +1,43 @@
 from unittest import TestCase
-from crawler.spider import Spider
+from mockito import when, mock, unstub
+from crawler.spider import Spider, LinkScraper
+from crawler.link import Link
 
 class SpiderTest(TestCase):
 
   def test_parse_all_links_in_page_for_the_given_url(self):
-    spider = Spider()
+    pass
+
+class LinkScraperTest(TestCase):
+  def test_parse_all_links_in_page_for_the_given_url(self):
+    page_url = "http://asamplepage.com"
+    html_content = "An html page with links"
+    page_links = [
+      Link(url="/about", label="A link", parent_url="http://asamplepage.com"),
+      Link("http://aurl.com", "A link", parent_url="http://asamplepage.com")
+    ]
+    mock_client = mock()
+    mock_parser = mock()
+    mock_document = mock()
+    when(mock_client).get_html_page(page_url).thenReturn(html_content)
+    when(mock_parser).parse(page_url, html_content).thenReturn(mock_document)
+    when(mock_document).get_links().thenReturn(page_links)
+    scraper = LinkScraper(mock_client, mock_parser)
+
+    links = scraper.scrape_links(page_url)
+
+    self.assertEquals(2, len(links))
+    self.assertCountEqual(page_links, links)
+
+  def test_parse_to_empty_links_when_the_url_does_not_return_a_html_page(self):
+    page_url = "http://nonexistent.com"
+    mock_client = mock()
+    mock_parser = mock()
+    mock_document = mock()
+    when(mock_client).get_html_page(page_url).thenReturn(None)
+    scraper = LinkScraper(mock_client, mock_parser)
+
+    links = scraper.scrape_links(page_url)
+
+    self.assertEquals(0, len(links))
 
